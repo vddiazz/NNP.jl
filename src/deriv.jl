84a709343d5115d6c89a580be7d88dd81a940b87
vddiazz <vddiazz@proton.me>
2026-06-03 12:16:09 +0200

    feat(deriv): exact derivatives wrt moduli

--- src/deriv.jl
+++ src/deriv.jl
@@ -1067,6 +1067,326 @@ end
 
 ### exact
 
+function deriv_x_exact(dir::Int64,grid_size::String,y1::Array{Float64},y2::Array{Float64},y3::Array{Float64},f_minus::Array{Float64},f_plus::Array{Float64}, df_minus::Array{Float64},df_plus::Array{Float64}, r_val::Float64,Q1::Matrix{ComplexF64}, Q2::Matrix{ComplexF64}, model::String)
+
+    #----- prepare params
+
+    @assert eltype(y1) == Float64 && eltype(y2) == Float64 && eltype(y3) == Float64
+    @assert eltype(f_plus) == Float64 && eltype(f_minus) == Float64 
+
+    sigma1 = [0im 1.; 1. 0im]
+    sigma2 = [0. -1im; 1im 0.]
+    sigma3 = [1. 0im; 0im -1.]
+	sigmas = [sigma1,sigma2,sigma3]
+
+    #----- prelocate arrays
+
+    l1 = length(y1); l2 = length(y2); l3 = length(y3)
+
+    y = zeros(Float64, 3)
+    xm = zeros(Float64, 3)
+
+    dirs1_1 = (Q1*sigma1)*inv(Q1); dirs1_2 = (Q1*sigma2)*inv(Q1); dirs1_3 = (Q1*sigma3)*inv(Q1)
+    dirs2_1 = (Q2*sigma1)*inv(Q2); dirs2_2 = (Q2*sigma2)*inv(Q2); dirs2_3 = (Q2*sigma3)*inv(Q2)
+
+    dirs_m = (Q1*sigma[dir])*inv(Q1)
+    dirs_p = (Q2*sigma[dir])*inv(Q2)
+
+    #----- evaluate field values
+    
+    dU_vals = zeros(Float64, l1,l2,l3,4)
+
+    @showprogress 1 "Computing..." for k in 1:l3
+        @inbounds @fastmath for j in 1:l2, i in 1:l1
+               
+            #----- params        
+
+            y[1] = y1[i]; y[2] = y2[j]; y[3] = y3[k]
+            xm[3] = r_val/2.
+            pos_p_1 = y[1]+xm[1]; pos_p_2 = y[2]+xm[2]; pos_p_3 = y[3]+xm[3]
+            pos_m_1 = y[1]-xm[1]; pos_m_2 = y[2]-xm[2]; pos_m_3 = y[3]-xm[3]
+
+            ar_p = sqrt(pos_p_1^2 + pos_p_2^2 + pos_p_3^2)
+            ar_m = sqrt(pos_m_1^2 + pos_m_2^2 + pos_m_3^2)
+
+            #----- f(r) values
+
+            f_p = f_plus[i,j,k]
+            f_m = f_minus[i,j,k]
+
+            cos_f_p = 1 - 0.5*f_p^2 + (1/24.)*f_p^4 - (1/720.)*f_p^6 + (1/40320.)*f_p^8 - (1/3638800.)*f_p^10
+            cos_f_m = 1 - 0.5*f_m^2 + (1/24.)*f_m^4 - (1/720.)*f_m^6 + (1/40320.)*f_m^8 - (1/3638800.)*f_m^10
+            sin_f_p = f_p - (1/6.)*f_p^3 + (1/120.)*f_p^5 - (1/5040.)*f_p^7 + (1/362880.)*f_p^9 - (1/39916800.)*f_p^11
+            sin_f_m = f_m - (1/6.)*f_m^3 + (1/120.)*f_m^5 - (1/5040.)*f_m^7 + (1/362880.)*f_m^9 - (1/39916800.)*f_m^11
+
+            #----- U1
+
+            Z1_11 = pos_m_1*(dirs1_1[1,1]) + pos_m_2*(dirs1_2[1,1]) + pos_m_3*(dirs1_3[1,1])
+            Z1_12 = pos_m_1*(dirs1_1[1,2]) + pos_m_2*(dirs1_2[1,2]) + pos_m_3*(dirs1_3[1,2])
+            Z1_21 = pos_m_1*(dirs1_1[2,1]) + pos_m_2*(dirs1_2[2,1]) + pos_m_3*(dirs1_3[2,1])
+            Z1_22 = pos_m_1*(dirs1_1[2,2]) + pos_m_2*(dirs1_2[2,2]) + pos_m_3*(dirs1_3[2,2])
+
+            # tr
+            tr_11 = sigma1[1,1]*Z1_11+sigma1[1,2]*Z1_21 + sigma1[2,1]*Z1_12+sigma1[2,2]*Z1_22
+            tr_21 = sigma2[1,1]*Z1_11+sigma2[1,2]*Z1_21 + sigma2[2,1]*Z1_12+sigma2[2,2]*Z1_22
+            tr_31 = sigma3[1,1]*Z1_11+sigma3[1,2]*Z1_21 + sigma3[2,1]*Z1_12+sigma3[2,2]*Z1_22
+
+            dirs1_param_1 = 0.5*(Z1_11+Z1_22); 
+            dirs1_param_2 = 0.5*tr_11; 
+            dirs1_param_3 = 0.5*tr_21; 
+            dirs1_param_4 = 0.5*tr_31
+
+            phi1_1 = cos_f_m + dirs1_param_1; 
+            phi1_2 = sin_f_m*(1/ar_m)*dirs1_param_2; 
+            phi1_3 = sin_f_m*(1/ar_m)*dirs1_param_3; 
+            phi1_4 = sin_f_m*(1/ar_m)*dirs1_param_4
+
+            #----- U2
+
+            Z2_11 = pos_p_1*(dirs2_1[1,1]) + pos_p_2*(dirs2_2[1,1]) + pos_p_3*(dirs2_3[1,1])
+            Z2_12 = pos_p_1*(dirs2_1[1,2]) + pos_p_2*(dirs2_2[1,2]) + pos_p_3*(dirs2_3[1,2])
+            Z2_21 = pos_p_1*(dirs2_1[2,1]) + pos_p_2*(dirs2_2[2,1]) + pos_p_3*(dirs2_3[2,1])
+            Z2_22 = pos_p_1*(dirs2_1[2,2]) + pos_p_2*(dirs2_2[2,2]) + pos_p_3*(dirs2_3[2,2])
+           
+            # tr
+            tr_12 = sigma1[1,1]*Z2_11+sigma1[1,2]*Z2_21 + sigma1[2,1]*Z2_12+sigma1[2,2]*Z2_22
+            tr_22 = sigma2[1,1]*Z2_11+sigma2[1,2]*Z2_21 + sigma2[2,1]*Z2_12+sigma2[2,2]*Z2_22
+            tr_32 = sigma3[1,1]*Z2_11+sigma3[1,2]*Z2_21 + sigma3[2,1]*Z2_12+sigma3[2,2]*Z2_22
+
+            dirs2_param_1 = 0.5*(Z2_11+Z2_22); 
+            dirs2_param_2 = 0.5*tr_12; 
+            dirs2_param_3 = 0.5*tr_22; 
+            dirs2_param_4 = 0.5*tr_32
+            
+            phi2_1 = cos_f_p + dirs2_param_1; 
+            phi2_2 = sin_f_p*(1/ar_p)*dirs2_param_2; 
+            phi2_3 = sin_f_p*(1/ar_p)*dirs2_param_3; 
+            phi2_4 = sin_f_p*(1/ar_p)*dirs2_param_4
+
+            #----- symmetrized product field
+
+            U_1 = real(phi1_1*phi2_1 - phi1_2*phi2_2 - phi1_3*phi2_3 - phi1_4*phi2_4)
+            U_2 = real(phi1_1*phi2_2 + phi1_2*phi2_1)
+            U_3 = real(phi1_1*phi2_3 + phi1_3*phi2_1)
+            U_4 = real(phi1_1*phi2_4 + phi1_4*phi2_1)
+
+            #----- normalization
+
+            C0 = U_1^2
+            Ck = U_2^2 + U_3^2 + U_4^2
+            N = sqrt(C0 + Ck)
+
+			#----- derivs
+
+			dr_m = -(y[dir]-xm[dir])/ar_m
+			dr_p = (y[dir]+xm[dir])/ar_p
+
+			df_m = df_minus[i,j,k] # PENDING
+			df_p = df_plus[i,j,k] # PENDING
+
+			drinv_m = -(1/ar_m^2)*dr_m
+			drinv_p = -(1/ar_p^2)*dr_p
+
+			dsin_m = cos_f_m*df_m*dr_m
+			dsin_p = cos_f_p*df_p*dr_p
+
+            dtr_m1 = sigma1[1,1]*dirs_m[1,1]+sigma1[1,2]*dirs_m[2,1] + sigma1[2,1]*dirs_m[1,2]+sigma1[2,2]*dirs_m[2,2]
+			dtr_m2 = sigma2[1,1]*dirs_m[1,1]+sigma2[1,2]*dirs_m[2,1] + sigma2[2,1]*dirs_m[1,2]+sigma2[2,2]*dirs_m[2,2]
+            dtr_m3 = sigma3[1,1]*dirs_m[1,1]+sigma3[1,2]*dirs_m[2,1] + sigma3[2,1]*dirs_m[1,2]+sigma3[2,2]*dirs_m[2,2]
+
+            dtr_p1 = sigma1[1,1]*dirs_p[1,1]+sigma1[1,2]*dirs_p[2,1] + sigma1[2,1]*dirs_p[1,2]+sigma1[2,2]*dirs_p[2,2]
+			dtr_p2 = sigma2[1,1]*dirs_p[1,1]+sigma2[1,2]*dirs_p[2,1] + sigma2[2,1]*dirs_p[1,2]+sigma2[2,2]*dirs_p[2,2]
+            dtr_p3 = sigma3[1,1]*dirs_p[1,1]+sigma3[1,2]*dirs_p[2,1] + sigma3[2,1]*dirs_p[1,2]+sigma3[2,2]*dirs_p[2,2]
+
+			#
+			
+			dphi1_1 = -sin_f_m*df_m*dr_m
+			dphi1_2 = drinv_m*sin_f_m*dirs_1_param_2 + (1/ar_m)*dsin_m*dirs_1_param_2 + (1/ar_m)*sin_f_m*dtr_m1
+			dphi1_3 = drinv_m*sin_f_m*dirs_1_param_3 + (1/ar_m)*dsin_m*dirs_1_param_3 + (1/ar_m)*sin_f_m*dtr_m2
+			dphi1_4 = drinv_m*sin_f_m*dirs_1_param_4 + (1/ar_m)*dsin_m*dirs_1_param_4 + (1/ar_m)*sin_f_m*dtr_m3
+
+			dphi2_1 = -sin_f_p*df_p*dr_p
+			dphi2_2 = drinv_p*sin_f_p*dirs_2_param_2 + (1/ar_p)*dsin_p*dirs_2_param_2 + (1/ar_p)*sin_f_p*dtr_p1
+			dphi2_3 = drinv_p*sin_f_p*dirs_2_param_3 + (1/ar_p)*dsin_p*dirs_2_param_3 + (1/ar_p)*sin_f_p*dtr_p2
+			dphi2_4 = drinv_p*sin_f_p*dirs_2_param_4 + (1/ar_p)*dsin_p*dirs_2_param_4 + (1/ar_p)*sin_f_p*dtr_p3
+
+			#----- deriv of phiS
+			
+			dphiS_1 = dphi1_1*phi2_1 + phi1_1*dphi2_1 - dphi1_2*phi2_2 - phi1_2*dphi2_2 - dphi1_3*phi2_3 - phi1_3*dphi2_3 - dphi1_4*phi2_4 - phi1_4*dphi2_4
+			dphiS_2 = dphi1_1*phi2_2 + phi1_1*dphi2_2 + dphi1_2*phi2_1 + phi1_2*dphi2_1
+			dphiS_3 = dphi1_1*phi2_3 + phi1_1*dphi2_3 + dphi1_3*phi2_1 + phi1_3*dphi2_1
+			dphiS_4 = dphi1_1*phi2_4 + phi1_1*dphi2_4 + dphi1_4*phi2_1 + phi1_4*dphi2_1
+
+			#----- deriv of normalization
+
+			dN = (U_1*dphiS_1 + U_2*dphiS_2 + U_3*dphiS_3 + U_4*dphiS_4)/(N^(1/2))
+
+            #----- return
+
+            dU_vals[i,j,k,1] = (dphiS_1*N - U_1*dN)/(N^2);
+			dU_vals[i,j,k,2] = (dphiS_2*N - U_2*dN)/(N^2);
+			dU_vals[i,j,k,3] = (dphiS_3*N - U_3*dN)/(N^2);
+			dU_vals[i,j,k,4] = (dphiS_4*N - U_4*dN)/(N^2);
+        
+        end
+    end
+
+    return dU_vals
+end
+
+
+function deriv_Q1_exact(dir::Int64,grid_size::String,y1::Array{Float64},y2::Array{Float64},y3::Array{Float64},f_minus::Array{Float64},f_plus::Array{Float64}, r_val::Float64,Q1::Matrix{ComplexF64}, Q2::Matrix{ComplexF64}, model::String)
+
+    #----- prepare params
+
+    @assert eltype(y1) == Float64 && eltype(y2) == Float64 && eltype(y3) == Float64
+    @assert eltype(f_plus) == Float64 && eltype(f_minus) == Float64 
+
+    sigma1 = [0im 1.; 1. 0im]
+    sigma2 = [0. -1im; 1im 0.]
+    sigma3 = [1. 0im; 0im -1.]
+	sigmas = [sigma1,sigma2,sigma3]
+
+    #----- prelocate arrays
+
+    l1 = length(y1); l2 = length(y2); l3 = length(y3)
+
+    y = zeros(Float64, 3)
+    xm = zeros(Float64, 3)
+
+    dirs1_1 = (Q1*sigma1)*inv(Q1); dirs1_2 = (Q1*sigma2)*inv(Q1); dirs1_3 = (Q1*sigma3)*inv(Q1)
+    dirs2_1 = (Q2*sigma1)*inv(Q2); dirs2_2 = (Q2*sigma2)*inv(Q2); dirs2_3 = (Q2*sigma3)*inv(Q2)
+	
+	ddirs1_1 = (Q1*(sigma1*sigmas[dir] - sigmas[dir]*sigma1))*inv(Q1); ddirs1_2 = (Q1*(sigma2*sigmas[dir] - sigmas[dir]*sigma2))*inv(Q1); ddirs1_3 = (Q1*(sigma3*sigmas[dir] - sigmas[dir]*sigma3))*inv(Q1)
+
+    #----- evaluate field values
+    
+    dU_vals = zeros(Float64, l1,l2,l3,4)
+
+    @showprogress 1 "Computing..." for k in 1:l3
+        @inbounds @fastmath for j in 1:l2, i in 1:l1
+               
+            #----- params        
+
+            y[1] = y1[i]; y[2] = y2[j]; y[3] = y3[k]
+            xm[3] = r_val/2.
+            pos_p_1 = y[1]+xm[1]; pos_p_2 = y[2]+xm[2]; pos_p_3 = y[3]+xm[3]
+            pos_m_1 = y[1]-xm[1]; pos_m_2 = y[2]-xm[2]; pos_m_3 = y[3]-xm[3]
+
+            ar_p = sqrt(pos_p_1^2 + pos_p_2^2 + pos_p_3^2)
+            ar_m = sqrt(pos_m_1^2 + pos_m_2^2 + pos_m_3^2)
+
+            #----- f(r) values
+
+            f_p = f_plus[i,j,k]
+            f_m = f_minus[i,j,k]
+
+            cos_f_p = 1 - 0.5*f_p^2 + (1/24.)*f_p^4 - (1/720.)*f_p^6 + (1/40320.)*f_p^8 - (1/3638800.)*f_p^10
+            cos_f_m = 1 - 0.5*f_m^2 + (1/24.)*f_m^4 - (1/720.)*f_m^6 + (1/40320.)*f_m^8 - (1/3638800.)*f_m^10
+            sin_f_p = f_p - (1/6.)*f_p^3 + (1/120.)*f_p^5 - (1/5040.)*f_p^7 + (1/362880.)*f_p^9 - (1/39916800.)*f_p^11
+            sin_f_m = f_m - (1/6.)*f_m^3 + (1/120.)*f_m^5 - (1/5040.)*f_m^7 + (1/362880.)*f_m^9 - (1/39916800.)*f_m^11
+
+            #----- U1
+
+            Z1_11 = pos_m_1*(dirs1_1[1,1]) + pos_m_2*(dirs1_2[1,1]) + pos_m_3*(dirs1_3[1,1])
+            Z1_12 = pos_m_1*(dirs1_1[1,2]) + pos_m_2*(dirs1_2[1,2]) + pos_m_3*(dirs1_3[1,2])
+            Z1_21 = pos_m_1*(dirs1_1[2,1]) + pos_m_2*(dirs1_2[2,1]) + pos_m_3*(dirs1_3[2,1])
+            Z1_22 = pos_m_1*(dirs1_1[2,2]) + pos_m_2*(dirs1_2[2,2]) + pos_m_3*(dirs1_3[2,2])
+
+            # tr
+            tr_11 = sigma1[1,1]*Z1_11+sigma1[1,2]*Z1_21 + sigma1[2,1]*Z1_12+sigma1[2,2]*Z1_22
+            tr_21 = sigma2[1,1]*Z1_11+sigma2[1,2]*Z1_21 + sigma2[2,1]*Z1_12+sigma2[2,2]*Z1_22
+            tr_31 = sigma3[1,1]*Z1_11+sigma3[1,2]*Z1_21 + sigma3[2,1]*Z1_12+sigma3[2,2]*Z1_22
+
+            dirs1_param_1 = 0.5*(Z1_11+Z1_22); 
+            dirs1_param_2 = 0.5*tr_11; 
+            dirs1_param_3 = 0.5*tr_21; 
+            dirs1_param_4 = 0.5*tr_31
+
+            phi1_1 = cos_f_m + dirs1_param_1; 
+            phi1_2 = sin_f_m*(1/ar_m)*dirs1_param_2; 
+            phi1_3 = sin_f_m*(1/ar_m)*dirs1_param_3; 
+            phi1_4 = sin_f_m*(1/ar_m)*dirs1_param_4
+
+            #----- U2
+
+            Z2_11 = pos_p_1*(dirs2_1[1,1]) + pos_p_2*(dirs2_2[1,1]) + pos_p_3*(dirs2_3[1,1])
+            Z2_12 = pos_p_1*(dirs2_1[1,2]) + pos_p_2*(dirs2_2[1,2]) + pos_p_3*(dirs2_3[1,2])
+            Z2_21 = pos_p_1*(dirs2_1[2,1]) + pos_p_2*(dirs2_2[2,1]) + pos_p_3*(dirs2_3[2,1])
+            Z2_22 = pos_p_1*(dirs2_1[2,2]) + pos_p_2*(dirs2_2[2,2]) + pos_p_3*(dirs2_3[2,2])
+           
+            # tr
+            tr_12 = sigma1[1,1]*Z2_11+sigma1[1,2]*Z2_21 + sigma1[2,1]*Z2_12+sigma1[2,2]*Z2_22
+            tr_22 = sigma2[1,1]*Z2_11+sigma2[1,2]*Z2_21 + sigma2[2,1]*Z2_12+sigma2[2,2]*Z2_22
+            tr_32 = sigma3[1,1]*Z2_11+sigma3[1,2]*Z2_21 + sigma3[2,1]*Z2_12+sigma3[2,2]*Z2_22
+
+            dirs2_param_1 = 0.5*(Z2_11+Z2_22); 
+            dirs2_param_2 = 0.5*tr_12; 
+            dirs2_param_3 = 0.5*tr_22; 
+            dirs2_param_4 = 0.5*tr_32
+            
+            phi2_1 = cos_f_p + dirs2_param_1; 
+            phi2_2 = sin_f_p*(1/ar_p)*dirs2_param_2; 
+            phi2_3 = sin_f_p*(1/ar_p)*dirs2_param_3; 
+            phi2_4 = sin_f_p*(1/ar_p)*dirs2_param_4
+
+            #----- symmetrized product field
+
+            U_1 = real(phi1_1*phi2_1 - phi1_2*phi2_2 - phi1_3*phi2_3 - phi1_4*phi2_4)
+            U_2 = real(phi1_1*phi2_2 + phi1_2*phi2_1)
+            U_3 = real(phi1_1*phi2_3 + phi1_3*phi2_1)
+            U_4 = real(phi1_1*phi2_4 + phi1_4*phi2_1)
+
+            #----- normalization
+
+            C0 = U_1^2
+            Ck = U_2^2 + U_3^2 + U_4^2
+            N = sqrt(C0 + Ck)
+
+			#----- deriv of phi1
+
+            Zd1_11 = pos_p_1*(ddirs1_1[1,1]) + pos_p_2*(ddirs1_2[1,1]) + pos_p_3*(ddirs1_3[1,1])
+            Zd1_12 = pos_p_1*(ddirs1_1[1,2]) + pos_p_2*(ddirs1_2[1,2]) + pos_p_3*(ddirs1_3[1,2])
+            Zd1_21 = pos_p_1*(ddirs1_1[2,1]) + pos_p_2*(ddirs1_2[2,1]) + pos_p_3*(ddirs1_3[2,1])
+            Zd1_22 = pos_p_1*(ddirs1_1[2,2]) + pos_p_2*(ddirs1_2[2,2]) + pos_p_3*(ddirs1_3[2,2])
+
+            tr_d11 = sigma1[1,1]*Zd1_11+sigma1[1,2]*Zd1_21 + sigma1[2,1]*Zd1_12+sigma1[2,2]*Zd1_22
+            tr_d21 = sigma2[1,1]*Zd1_11+sigma2[1,2]*Zd1_21 + sigma2[2,1]*Zd1_12+sigma2[2,2]*Zd1_22
+            tr_d31 = sigma3[1,1]*Zd1_11+sigma3[1,2]*Zd1_21 + sigma3[2,1]*Zd1_12+sigma3[2,2]*Zd1_22
+
+            ddirs1_param_2 = 0.25*1im*tr_d11; 
+            ddirs1_param_3 = 0.25*1im*tr_d21; 
+            ddirs1_param_4 = 0.25*1im*tr_d31
+
+			dphi1_2 = sin_f_p*(1/ar_p)*ddirs1_param_2;
+			dphi1_3 = sin_f_p*(1/ar_p)*ddirs1_param_3;
+			dphi1_4 = sin_f_p*(1/ar_p)*ddirs1_param_4;
+
+			#----- deriv of phiS
+			
+			dphiS_1 = - dphi1_2*phi2_2 - dphi1_3*phi2_3 - dphi1_4*phi2_4;
+			dphiS_2 = dphi1_2*phi2_1;
+			dphiS_3 = dphi1_3*phi2_1;
+			dphiS_4 = dphi1_4*phi2_1
+
+			#----- deriv of normalization
+
+			dN = (U_1*dphiS_1 + U_2*dphiS_2 + U_3*dphiS_3 + U_4*dphiS_4)/(N^(1/2))
+
+            #----- return
+
+            dU_vals[i,j,k,1] = (dphiS_1*N - U_1*dN)/(N^2);
+			dU_vals[i,j,k,2] = (dphiS_2*N - U_2*dN)/(N^2);
+			dU_vals[i,j,k,3] = (dphiS_3*N - U_3*dN)/(N^2);
+			dU_vals[i,j,k,4] = (dphiS_4*N - U_4*dN)/(N^2);
+        
+        end
+    end
+
+    return dU_vals
+end
+
+
 function deriv_Q2_exact(dir::Int64,grid_size::String,y1::Array{Float64},y2::Array{Float64},y3::Array{Float64},f_minus::Array{Float64},f_plus::Array{Float64}, r_val::Float64,Q1::Matrix{ComplexF64}, Q2::Matrix{ComplexF64}, model::String)
 
     #----- prepare params
@@ -1196,10 +1516,10 @@ function deriv_Q2_exact(dir::Int64,grid_size::String,y1::Array{Float64},y2::Arra
 
 			#----- deriv of phiS
 			
-			dphiS_1 = phi1_1*phi2_1 - phi1_2*dphi2_2 - phi1_3*dphi2_3 - phi1_4*dphi2_4;
-			dphiS_2 = phi1_1*dphi2_1 + phi1_1*phi2_1;
-			dphiS_3 = phi1_1*dphi2_2 + phi1_2*phi2_1;
-			dphiS_4 = phi1_1*dphi2_3 + phi1_3*phi2_1
+			dphiS_1 = - phi1_2*dphi2_2 - phi1_3*dphi2_3 - phi1_4*dphi2_4;
+			dphiS_2 = phi1_1*dphi2_2;
+			dphiS_3 = phi1_1*dphi2_3;
+			dphiS_4 = phi1_1*dphi2_4
 
 			#----- deriv of normalization
 
